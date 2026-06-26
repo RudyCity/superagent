801677bc7e38fcee3b5d5bf9433850d040995702
Test <test@test.com>
2026-06-26 22:34:29 +0700

    feat: add /memory slash command for real-time TencentDB memory management and diagnostics

--- dist/core/commands/index.d.ts
+++ dist/core/commands/index.d.ts
@@ -4,6 +4,7 @@ import "./configCommands.js";
 import "./agentCommands.js";
 import "./skillCommands.js";
 import "./internalHooksCommand.js";
+import "./memoryCommand.js";
 export { registry } from "./registry.js";
 export * from "./types.js";
 //# sourceMappingURL=index.d.ts.map
---
+++ dist/core/commands/memoryCommand.d.ts
@@ -0,0 +1,3 @@
+import { SlashCommand } from "./types.js";
+export declare const memoryCommand: SlashCommand;
+//# sourceMappingURL=memoryCommand.d.ts.map
